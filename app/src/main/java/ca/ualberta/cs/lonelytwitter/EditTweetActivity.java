d316addb7cce58af14d6f706c68cc57907039ce8
bcvancouver <michael-xi@hotmail.com>
2015-10-13 13:01:17 -0600

    Working on EditTweet

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/EditTweetActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/EditTweetActivity.java
@@ -4,13 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditTweetActivity extends Activity {
+    private EditText textBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_tweet);
+        textBox=new EditText(this);
+        textBox=(EditText)findViewById(R.id.textBox);
+        textBox.setText("hamburger");
     }
 
     @Override
